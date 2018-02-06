f6d17091abd0145347254492992c7fa83094ae9d
scarlet-chariot <alex.ling.tseng@gmail.com>
2018-02-05 22:12:54 -0700

    update

--- Thomas.py
+++ Thomas.py
@@ -56,21 +56,61 @@ for i in range (0,2):
 thomas.end_fill()
 thomas.penup()
 
+# Background 3 of thomas
+thomas.goto(-20,-5)
+thomas.pendown()
+thomas.fillcolor("black")
+thomas.begin_fill()
+for i in range (0,2):
+        thomas.forward(40)
+        thomas.rt(90)
+        thomas.fd(35)
+        thomas.rt(90)
+thomas.end_fill()
+thomas.penup()
+
+# Background 4 of thomas
+thomas.goto(-15,-40)
+thomas.pendown()
+thomas.fillcolor("lightgrey")
+thomas.begin_fill()
+for i in range(0,2):
+        thomas.fd(30)
+        thomas.lt(90)
+        thomas.fd(18)
+        thomas.lt(90)
+thomas.end_fill()
+thomas.penup()
+
+# Background 5 of thomas
+thomas.goto(-30,-5)
+thomas.pendown()
+thomas.fillcolor("black")
+thomas.begin_fill()
+for i in range(0,2):
+        thomas.fd(60)
+        thomas.lt(90)
+        thomas.fd(5)
+        thomas.lt(90)
+thomas.end_fill()
+thomas.penup()
+
+
 # Even more bottom rail thing
-thomas.goto(-60,-60)
+thomas.goto(-60,-50)
 thomas.pendown()
 thomas.fillcolor("red")
 thomas.begin_fill()
 for i in range (0,2):
 	thomas.forward(120)
 	thomas.left(90)
-	thomas.forward(20)
+	thomas.forward(10)
 	thomas.left(90)
 thomas.end_fill()
 thomas.penup()
 
 # Left Wheel
-thomas.goto(-40,-70)
+thomas.goto(-40,-55)
 thomas.pendown()
 thomas.fillcolor("black")
 thomas.begin_fill()
@@ -81,7 +121,7 @@ thomas.end_fill()
 thomas.penup()
 
 # Right Wheel
-thomas.goto(40,-70)
+thomas.goto(35,-55)
 thomas.pendown()
 thomas.fillcolor("black")
 thomas.begin_fill()
@@ -130,55 +170,56 @@ thomas.end_fill()
 thomas.penup()
 
 # Left Dot Freckle thing
-thomas.goto(-30,40)
+thomas.setheading(0)
+thomas.goto(-20,35)
 thomas.pendown()
-thomas.fillcolor("black")
+thomas.fillcolor("yellow")
 thomas.begin_fill()
 for i in range(0,8):
-	thomas.lt(45)
 	thomas.fd(5)
+	thomas.lt(45)
 thomas.end_fill()
 thomas.penup()
 
 # Right Dot Freckle thing
-thomas.setheading(0)
-thomas.goto(30,40)
+thomas.setheading(180)
+thomas.goto(20,35)
 thomas.pendown()
-thomas.fillcolor("black")
+thomas.fillcolor("yellow")
 thomas.begin_fill()
 for i in range(0,8):
-	thomas.lt(45)
 	thomas.fd(5)
+	thomas.rt(45)
 thomas.end_fill()
 thomas.penup()
 
 # Left Inner Dot Freckle thing
 thomas.setheading(0)
-thomas.goto(-30,45)
+thomas.goto(-19,37.5)
 thomas.pendown()
-thomas.fillcolor("yellow")
+thomas.fillcolor("black")
 thomas.begin_fill()
 for i in range(0,8):
+	thomas.fd(3)
 	thomas.lt(45)
-	thomas.fd(5)
 thomas.end_fill()
 thomas.penup()
 
 # Right Inner Dot Freckle thing
-thomas.setheading(0)
-thomas.goto(30,45)
+thomas.setheading(180)
+thomas.goto(19,37.5)
 thomas.pendown()
-thomas.fillcolor("yellow")
+thomas.fillcolor("black")
 thomas.begin_fill()
 for i in range(0,8):
-	thomas.lt(45)
-	thomas.fd(5)
+	thomas.fd(3)
+	thomas.rt(45)
 thomas.end_fill()
 thomas.penup()
 
 # Face
 thomas.setheading(0)
-thomas.goto(0,-10)
+thomas.goto(-8,-10)
 thomas.pendown()
 thomas.fillcolor("lightgrey")
 thomas.begin_fill()
@@ -190,25 +231,25 @@ thomas.penup()
 
 # Left Sclera
 thomas.setheading(0)
-thomas.goto(-10,15)
+thomas.goto(-12,12.5)
 thomas.pendown()
 thomas.fillcolor("white")
 thomas.begin_fill()
 for i in range (0,10):
-	thomas.fd(15)
+	thomas.fd(5)
 	thomas.lt(36)
 thomas.end_fill()
 thomas.penup()
 
 # Right Sclera
-thomas.setheading(0)
-thomas.goto(10,15)
+thomas.setheading(180)
+thomas.goto(12,12.5)
 thomas.pendown()
 thomas.fillcolor("white")
 thomas.begin_fill()
 for i in range (0,10):
-	thomas.fd(15)
-	thomas.lt(36)
+	thomas.fd(5)
+	thomas.rt(36)
 thomas.end_fill()
 thomas.penup()
 
@@ -223,8 +264,7 @@ thomas.end_fill()
 thomas.penup()
 
 # Right Pupil
-thomas.setheading(0)
-thomas.goto(-10,17)
+thomas.goto(10,17)
 thomas.pendown()
 thomas.fillcolor("black")
 thomas.begin_fill()
@@ -233,19 +273,19 @@ thomas.end_fill()
 thomas.penup()
 
 # Nose
-thomas.setheading(0)
-thomas.goto(-2,25)
+thomas.goto(2,15)
 thomas.pendown()
 for i in range(0,9):
-	thomas.lt(36)
+	thomas.rt(36)
 	thomas.fd(3)
 thomas.penup()
 
 # Smile
-thomas.goto(0,5)
+thomas.setheading(270)
+thomas.goto(-5,5)
 thomas.pendown()
 thomas.circle(5,180)
 
 # Finishes
 thomas.ht()
-thomas.exitonclick()
+wn.exitonclick()
