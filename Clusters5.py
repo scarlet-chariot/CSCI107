059a42ddce78d84d8558b8480f73204abfd87f63
scarlet-chariot <alex.ling.tseng@gmail.com>
2018-04-24 01:05:47 -0600

    update

--- Clusters5.py
+++ Clusters5.py
@@ -6,55 +6,59 @@
 #		   cluster for Gaussian.
 # Last Updated: April 23, 2018
 #------------------------------------------------
-# Patch Number: 0.5
-# Patch Notes: Remade from Clusters4.py.
-#			   Creates temporary files before
-#			   transfering to new file.
+# Patch Number: 0.4
+# Patch Notes: Remade from Clusters3.py.
+#			   Added definitions to shorten
+#   		   the length of code.
 #------------------------------------------------
 
-import tempfile
 pdb_file= input("Please enter the pdb file > \n")
 pdb_file_out= input("Please enter/name the output file > \n")
-natoms= int(0)
-
-# copies all the data from pdb_file and changes it so that everything is now 1 space apart.
-temporary= tempfile.TemporaryFile()
-with open(pdb_file) as f:
-	many_space= "  "
-	for lines in f:
-		while many_space in lines:
-			new_temporary_line= lines.split("  ")
-			lines= " ".join(new_temporary_line)
-		temporary.write(lines)
-# to debug, uncomment this:
-#		temporary.read()
 
 # definitions:
+# remove spaces:
+def rmspace():
+	many_space="  "
+	global lines
+	while many_space in lines:
+		new_temporary_line= lines.split("  ")
+		lines= " ".join(new_temporary_line)
+
 # copy the wanted data to the new file:
-def copylines(b,c,d):
-	if d != "":
-		e= b+" "+d
-		for lines in temporary:
-			if d in lines:
-				broken_line= lines.split(" ")
-				atom_identity= broken_line[:2]
-				x_coor= broken_line[:5]
-				y_coor= broken_line[:6]
-				z_coor= broken_line[:7]
-				new_line= str(atom_identity, x_coor, y_coor, z_coor, "\n")
-				keep_file.write(new_line)
+def copylines():
+	rmspace()
+	global lines
+	atom_identity= str(lines.split(" ")[2])
+	atom_identity= atom_identity[:1]
+	x_coor= str(lines.split(" ")[5])
+	y_coor= str(lines.split(" ")[6])
+	z_coor= str(lines.split(" ")[7])
+	new_line= atom_identity+" "+x_coor+" "+y_coor+" "+z_coor
+	keep_file.write(new_line)
+	keep_file.write("\n")
 
-			
-# user inputs
-def user_input():
-	a= input("How much to copy? (All residues + All substrates + All cofactors)")
+#-------------------------------------------------
+#Start of program
+number= int(input("How many to copy? (All residues + All substrates + All cofactors) > \n"))
+with open(pdb_file) as original_file:
 	with open(pdb_file_out, "a+") as keep_file:
-	for i in range(a):
-		b= input("What to copy? Residue/Substrate/Cofactor > \n")
-		b= b.lower()
-		c= input("What is its identity? (Must be abbreviated the same as in the pdb file.) > \n")
-		if b == "residue":
-			d= input("What is the residue number? > \n")
-
-user_input()
-
+		for i in range(number):
+			choice= input("What to copy? (Residue/Substrate/Cofactor) > \n")
+			choice= choice.lower()
+			while not (choice == "residue" or choice == "substrate" or choice == "cofactor"):
+				choice= input("What to copy? (Residue/Substrate/Cofactor) > \n")
+			identity= input("What is its identity? (Must be abbreviated the same as in the pdb file.) > \n")
+			identity= identity.upper()
+			if choice == "residue":
+				identity_number= input("What is the residue number? > \n")
+				identity_number= " "+identity_number+" "
+				original_file.seek(0)
+				for lines in original_file:
+					if identity in lines and identity_number in lines:
+						copylines()
+			elif choice == "substrate" or choice == "cofactor":
+				check= " "+identity+" "
+				original_file.seek(0)
+				for lines in original_file:
+					if check in lines:
+						copylines()
